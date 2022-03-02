d5a0c097b8578f44015f4846a494dc06cc1e7e34
Hyesung Park <jayhspark@gmail.com>
2022-03-02 18:19:49 +0900

    update

---
+++ lyrics-search/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(
+  "/static",
+  express.static(path.resolve(__dirname, "./public"), {
+    extensions: ["js", "css"],
+  })
+);
+
+
+const port = 3321
+
+app.get("/*",(req,res) => {
+    res.sendFile(path.resolve("./src/static", "./index.html"))
+} )
+
+
+app.listen(port, () => console.log("server =>" + port))
