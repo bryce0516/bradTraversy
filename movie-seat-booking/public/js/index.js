2f05f97c9aa7a10f779b9e1fa1e075658461d0c1
Hyesung Park <jayhspark@gmail.com>
2022-03-02 12:34:55 +0900

    update

--- movie-seat-booking/public/js/index.js
+++ movie-seat-booking/public/js/index.js
@@ -1,1 +1,11 @@
-console.log("working")
+
+const container = document.querySelector('.container')
+const seat = document.querySelectorAll('.row .seat:not(.occupied)')
+const count = document.getElementById('count')
+const total = document.getElementById('total')
+
+const movieSelect = document.getElementById('movie')
+
+const ticketPrice = parseInt(movieSelect.value);
+
+console.log(seat,ticketPrice, typeof ticketPrice)
